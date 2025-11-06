690eda70fad13e614b713340d2c426aaf06b3b78
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-06 11:04:43 +0000

    Variablen vs. Konstanten in JS

--- src/02_Variablen.js
+++ src/02_Variablen.js
@@ -27,24 +27,37 @@
     JS ist ein untypisierte Sprache!
 */
 
-let test;
+//let test;
 
 /* undefined */
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------");
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------");
 
-test = "hi"; // string
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------");
+//test = "hi"; // string
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------");
 
-test = 2; // number
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------");
+//test = 2; // number
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------");
 
-test = true; // boolean
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------");
+//test = true; // boolean
+//console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+//console.log("Inhalt: " + test); // Ausgabe Inhalt
+//console.log("----------------");
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let Test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initsialisierung)
+test = "hello"; // überschreiben
+console.log("inhalt: " + test); // Ausgabe
+
+// Konstante
+const test2 = "Hi!"; // Deklaration + Wertzuwweisung gemeinsam
+test2 ="hello"; // Laufzeitfehler
+console.log("Inhalt: " +test2); // Ausgabe 
