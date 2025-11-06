cbc9c5dc698ff2b4fb360de910d0cfdb79dbcc79
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-06 10:53:53 +0000

    verschiedene Variablen

--- src/02_Variablen.js
+++ src/02_Variablen.js
@@ -7,3 +7,44 @@
  * 
  */
 /******* Variablen 01 *********/
+
+//console.log("Hi!");
+//console.log("Hi!");
+//console.log(Hi);
+
+/* Deklaration + Wertzuweisung I */
+
+//let firstName; // Deklaration (Definition)
+
+//console.log(firstName); // Ausgabe
+//firstName = "Max"; // Wertzuweisung / assignment
+//console.log("Inhalt: " + firstName); // Ausgabe
+//let familyName = "Mütze"; // Deklartation & Wertzuweisung
+//console.log("Hi, ich bin " + firstName + " " + familyName + "!");
+
+/*  
+    Wertzuweisung II + Datentypen 
+    JS ist ein untypisierte Sprache!
+*/
+
+let test;
+
+/* undefined */
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------");
+
+test = "hi"; // string
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------");
+
+test = 2; // number
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------");
+
+test = true; // boolean
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------");
