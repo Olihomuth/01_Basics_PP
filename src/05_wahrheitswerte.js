707aaf95d823bdbba40a62ccd2de3ef5cf524a03
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-07 10:44:50 +0000

    05 / Wahrheitswerte anpassen

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,27 @@
+
+/* Berechnung Alter */
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+
+// Berechnung Alter
+// let year = 2025;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter Iteration II
+let date = new Date();
+let year = date.getFullYear();
+
+console.log("Datum: " + date);
+console.log("Jahr: " + year);
+
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
