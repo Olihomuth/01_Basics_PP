b702510979c7bb5a2cddfbb01e9eea33503d75b4
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-07 09:52:31 +0000

    04 Lösung

--- src/04_codechallenge_I.js
+++ src/04_codechallenge_I.js
@@ -10,3 +10,7 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 const prompt = require('prompt-sync')({sigint: true});
+let firstNumber = parseInt(prompt("Erste Zahl: "));
+let secondNumber = parseInt(prompt("Zweite Zahl: "));
+let summe = firstNumber + secondNumber; 
+console.log("Die Summe der Zahlen ist: " , summe); 
