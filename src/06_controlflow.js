07754e9c2d3408a6b449afc1fcbd17036a67d52f
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-10 08:53:26 +0000

    06 / IF - ELSE

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,7 +1,7 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 30;
+const ageJohn = 25;
 const ageMark = 30;
 
 // Deklaration
@@ -12,17 +12,36 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
+ console.log("ageJohn: " + ageJohn);
+ console.log("ageMark: " + ageMark);
 // console.log("isJohnOlder: " + isJohnOlder);
 // console.log("isJohnEqual: " + isJohnEqual);
 // console.log("------------------");
 
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+
 // if(true)             // Wahrheitswerte
 // if(false)            
 // if(isJohnOlder)      // Verweis
-if(ageJohn > ageMark)   // direkter Test
-{
-   console.log("John is älter."); 
-}
+// if(ageJohn > ageMark)   // direkter Test
+// {
+//    console.log("John is älter."); 
+// }
 
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+// if(true)
+// if(false)
+// if(isJohnolder)
+if (ageJohn > ageMark) {
+    console.log("John is älter.");   
+} else {
+    console.log("John is jünger.");
+}    
