57296437749979f41b89fc4dd554d281a3addd90
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-10 11:52:21 +0000

    07 / Codechallenge 2

---
+++ src/07_codechallenge_II.js
@@ -0,0 +1,46 @@
+/*********   CODE CHALLENGE 02 **********/
+
+/*
+Geben über eine Prompt-Anforderung einen Namen ein
+Geben über eine Prompt-Anforderung ein Alter ein
+
+Geben Sie in Abhängigkeit vom angegebenen Alter 
+folgende Getränkeauswahl in die Konsole aus :
+
+Alter 0-5 // Milch  --> age > 0 AND age < 6
+Alter 6-12 // Saft
+Alter 13-17 // Cola
+18 + // Wein
+
+-->zB: "Max trinkt Cola."
+
+/* Getränke-Challenge */
+
+
+const prompt = require('prompt-sync')({sigint: true});
+
+let firstName, age;
+
+firstName = prompt("ihr Name Lautet?: ");
+age = parseInt(prompt("Ihr Alter ist?: "));
+
+const cond = true;
+
+switch (cond) {
+    case (age >=0) && (age <= 5):
+        console.log(firstName, "Sie bekommen Milch!");
+        break;
+    case (age >= 6) && (age <= 12):
+        console.log(firstName, "Sie bekommen Saft!");
+        break;
+    case (age >= 13) && (age <= 17):
+        console.log(firstName, "Sie bekommen Cola!");
+        break;
+    case (age >= 18) && (age <= 130):
+        console.log(firstName, "Sie bekommen Wein!");
+        break;
+
+    default:
+        console.log(firstName, "Bitte Tee trinken!");
+        break;
+}
