0bd1d0cf11077d17a1c07980a83dd3993afa07ed
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-13 11:41:16 +0000

    11 / Transponierung Theorie und Praxis mit Arrays

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -43,14 +43,53 @@ for (let i = 0; i < 10 ; i--) {
  */
 
 /* 02b. For-Schleife für Array-Index (Iteration)*/
-let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+/* let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
 for (let i = 0; i < arr.length; i++) 
 {
     output(i); // interner Index 
     output(arr[i]);  // Mapping  intern --> extern
-}
+} */
 
 
+
+/********* Überlegungen - Transponierung **********/
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/* Zu "Fuß" - DRY !!! */
+/* let a = 0; // Anfangswert
+output("inhalt von a: " + a);
+a = a + 1; // a = 0 + 1
+output("inhalt von a: " + a);
+a = a + 1; // a = 1 + 1
+output("inhalt von a: " + a);
+a = a + 1; // a = 2 + 1
+output("inhalt von a: " + a); */
+
+/* Besser: mit FOR-Schleife */
+
+/* let a = 0; // Anfangswert
+for (let i = 0; i < 10; i++) {
+   // a = a + 1
+    a += 1;  
+    output("inhalt von a: " + a);
+} */
+
+
+/* 1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+ */
+
+/* let str = ""; // Anfangswert
+for (let i = 0; i < 10; i++) {
+   str += "Test" + " "; // a = a + 1
+   output(str);
+} */
 
 
 
@@ -102,7 +141,45 @@ function getSentenceArr(arr) {
     return str;
 }
 
+/*** 01b. Funktionalität mit Array 2  */
 
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananananderdereihung ...
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) {
+
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = "";
+
+    // 1. Iteration: Struktur GENAU erzeugen ...
+/*     for (let i = 0; i < arr.length; i++) {
+        if (i != arr.length - 1){
+            str += arr[i] + GAP;
+        } else {
+            str += arr[i] + PUNCT;
+        }
+    }
+
+    return str;
+*/
+
+
+
+
+    // 2. Iteration: Struktur erzeugen + trimmen()
+/*         for (let i = 0; i < arr.length; i++) {
+            str += arr[i] + GAP; 
+        }
+        
+    return str.trim() + PUNCT; */
+
+    // 3. Variante: Ausnutzen von generischen Fuktionen ...
+    
+    return arr.join(GAP) + PUNCT;
+
+
+}
 
 
 
