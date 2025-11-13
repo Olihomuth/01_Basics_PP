df83f6c35d600e6ece9e889a795691a7bb900adb
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-13 10:00:09 +0000

    11 / Arrays Theorie

---
+++ src/11_arrays_I.js
@@ -0,0 +1,55 @@
+
+/* Die Satzbau-Maschine | Arrays */
+
+/**
+ *   THEORIE
+ * 
+ */
+
+/*** 02. Theorie: Array */
+
+let arr; 
+// arr = new Array();   // Konstructor
+arr = [];
+arr = [2,11,7,8,10,20];
+
+output(arr);
+output(arr.length);             // Anzahl der Elemente
+output(arr[0]);                 // Index 0 (1.Pos. im Array)
+output(arr[arr.length-1]);      // lezte Pos. / allgemein  
+
+
+/**
+ *   PRAXIS
+ * 
+ */
+
+
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4, word5, word6) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP +
+                word2 + GAP +
+                word3 + GAP +
+                word4 + GAP + 
+                word5 + GAP + 
+                word6 +  
+                PUNCT;
+
+    return str;
+}
+
+
+
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(outputStr) {
+    console.log(outputStr);
+}
+
+
