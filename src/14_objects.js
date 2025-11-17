d5d335f1ccbb4784d9e43d182bde04b9e1d7a9bb
Olihomuth <159420281+Olihomuth@users.noreply.github.com>
2025-11-17 10:01:44 +0000

    14 / Hierachie mit nested object

--- src/14_objects.js
+++ src/14_objects.js
@@ -30,26 +30,64 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
-let person = {
-                firstName: "Oliver", 
-                familyName: "Homuth",
-                salary: [120000, 160000],
-                permission: true, 
+// let person = {
+//                 firstName: "Oliver", 
+//                 familyName: "Homuth",
+//                 salary: [120000, 160000],
+//                 permission: true, 
+//                 sayHello: function(){
+//                     return "Hallo";
+//                 },
+//                 sayHello2: function() {
+//                     return "Hallo, ich bin " + this.firstName
+//                 }
 
-            };
-output(person);
-output(person.firstName);
-output(person.familyName);
+//             };
+// output(person);
+// output(person.firstName);
+// output(person.familyName);
 
-output("------------");
-output(person.salary[1]);
-output(person.salary.length);
-for (let i = 0; i < person.salary.length; i++); {
-    output(person.salary[i]);
-}
-output("------------");
+// output("------------");
+// output(person.salary[1]);
+// output(person.salary.length);
+// for (let i = 0; i < person.salary.length; i++) {
+//     output(person.salary[i]);
+// }
 
-output(person.permission);
+// output(person.permission);
+
+// const txt = person.sayHello() + ", ich bin " + person.firstName +
+
+//          " und verdiene " + person.salary[1] + " p.a."
+ 
+// output(txt);
+// output("------------");
+
+// const txt2 = person.sayHello2() + " und verdiene " + person.salary[1] + " p.a.";
+// output(txt2);
+
+
+
+/***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der  gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+// nested object {{}}
+
+let baikal = {
+                value:"10m",
+                deep:{
+                    deeper:{
+                            temperature:"4 Drad Celcius",
+                            deepest:"Das Licht - auf 1642m!"
+                            }
+                    }
+            }
+
+// output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.temperature);
+output(baikal.deep.deeper.deepest);
 
 
 
